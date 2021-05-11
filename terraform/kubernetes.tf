2021ec307c83dbd1c07d71a1027b780c408acdbb
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2021-05-11 10:11:52 +0200

    🔧(terraform) declare a managed k8s cluster and a nodepool
    We want to deploy a managed Kubernetes cluster and a nodepool with
    terraform.

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,14 @@
+
+resource "ovh_cloud_project_kube" "kube_cluster" {
+   name         = var.k8s_cluster_name
+   region       = var.k8s_cluster_region
+}
+
+resource "ovh_cloud_project_kube_nodepool" "kube_nodepool" {
+   kube_id       = ovh_cloud_project_kube.kube_cluster.id
+   name          = "jitsipool"
+   flavor_name   = var.k8s_nodepool_flavor
+   max_nodes     = var.k8s_nodepool_max_nodes
+   min_nodes     = var.k8s_nodepool_min_nodes
+   autoscale     = var.k8s_nodepool_autoscale
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,44 @@ variable "ovh_endpoint" {
   default = "ovh-eu"
   description = "The OVH API endpoint to use"
 }
+
+# OVH managed Kubernetes cluster settings
+
+variable "k8s_cluster_name" {
+  type = string
+  default = "jitsi"
+  description = "The name of the k8s cluster"
+}
+
+variable "k8s_cluster_region" {
+  type = string
+  default = "GRA7"
+  description = "The OVH region in which the k8s cluster will be created."
+}
+
+
+# Kubernetes nodepool settings
+
+variable "k8s_nodepool_autoscale" {
+  type = bool
+  default = false
+  description = "Enables the pool autoscaling feature"
+}
+
+variable "k8s_nodepool_flavor" {
+  type = string
+  description = "Flavor name of the instances that will be created for the node pool"
+  default = "b2-7"
+}
+
+variable "k8s_nodepool_min_nodes" {
+  type = number
+  default = 1
+  description = "Minimum number of nodes allowed in the node pool"
+}
+
+variable "k8s_nodepool_max_nodes" {
+  type = number
+  default = 1
+  description = "Maximum number of nodes allowed in the pool"
+}
