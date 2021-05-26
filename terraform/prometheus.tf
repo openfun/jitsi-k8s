8c4f74bcda296e7e66fdf8073423e49f5e5db896
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2021-05-26 18:54:50 +0200

    🐛(terraform) fix dependency graph for resources creation
    When executing `bin/terraform apply` from scratch, the helm tasks are
    failing because they are not executed in the right order. Terraform is
    not able to infer the correct order of creation of the declared
    resources.

--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -7,6 +7,8 @@ resource "helm_release" "kube-prometheus-stack" {
 
   repository = "https://prometheus-community.github.io/helm-charts"
   chart = "kube-prometheus-stack"
+
+  depends_on = [ ovh_cloud_project_kube_nodepool.kube_nodepool, local_file.kubeconfig]
 }
 
 
@@ -19,6 +21,8 @@ resource "helm_release" "prometheus-adapter" {
 
   repository = "https://prometheus-community.github.io/helm-charts"
   chart = "prometheus-adapter"
+
+  depends_on = [ helm_release.kube-prometheus-stack ]
 }
 
 # Install Kube-eagle with helm to have a practical Grafana dashboard
@@ -41,4 +45,6 @@ resource "helm_release" "kube-eagle" {
     name = "serviceMonitor.releaseLabel"
     value = "kube-prometheus-stack"
   }
+
+  depends_on = [ helm_release.kube-prometheus-stack ]
 }
