fe8072314e9b51aac77a1ad4ef9c1439e0a61566
Matthieu Huguet <matthieu.huguet@fun-mooc.fr>
2021-07-15 18:18:54 +0200

    ✨(k8s) use separate nodepool for jibri
    At this time, we create a single nodepool on which we schedule all our
    pods. Some of them are related to jibri, while others are related to
    prometheus or, in a near future, other jitsi components.
    
    Some pods can prevent the cluster autoscaler to remove a jibri node,
    for example if they use local storage. (see:
    https://github.com/kubernetes/autoscaler/blob/master/cluster-autoscaler/FAQ.md#what-types-of-pods-can-prevent-ca-from-removing-a-node)
    
    To avoid wasting resources while using the cluster autoscaler feature,
    we want to isolate jibri pods on a specific nodepool, with autoscaling
    enabled. And use another pool for resources that don't need
    autoscaling.

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -9,20 +9,53 @@ resource "scaleway_k8s_cluster" "kube_cluster" {
       maintenance_window_start_hour = lookup(var.k8s_auto_upgrade_maintenance_window_start_hour, terraform.workspace, 3)
       maintenance_window_day = lookup(var.k8s_auto_upgrade_maintenance_window_day, terraform.workspace, "any")
    }
-
 }
 
-resource "scaleway_k8s_pool" "kube_nodepool" {
+# In this cluster, we create 2 nodepools :
+#
+# - The `default` nodepool, on which we'll deploy all pods that do not require
+#   autoscaling feature (e.g. the prometheus stack and basic jitsi components
+#   like prosody and jitsi-meet webserver). It is desirable to isolate them from
+#   nodepools with autoscaling enabled, as they can prevent the Cluster Autoscaler
+#   from removing a node if they run on them.
+#
+# - The `jibri` nodepool, dedicated to jibri instances, with autoscaling enabled.
+#
+# In the future, we'll propably add a nodepool dedicated to videobridges.
+#
+# This separation allows to choose instance types adapted to the needs of each
+# component (e.g. : Jibri consumes a lot of CPU/RAM, Videobridges consume a lot of
+# bandwdith).
+
+
+resource "scaleway_k8s_pool" "default" {
    autohealing         = lookup(var.k8s_nodepool_autohealing, terraform.workspace, true)
-   autoscaling         = lookup(var.k8s_nodepool_autoscale, terraform.workspace, false)
+   autoscaling         = false
    cluster_id          = scaleway_k8s_cluster.kube_cluster.id
    container_runtime   = lookup(var.k8s_nodepool_container_runtime, terraform.workspace, "containerd")
-   max_size            = lookup(var.k8s_nodepool_max_nodes, terraform.workspace, 1)
-   min_size            = lookup(var.k8s_nodepool_min_nodes, terraform.workspace, 1)
-   name                = "jitsi-${terraform.workspace}"
-   node_type           = lookup(var.k8s_nodepool_flavor, terraform.workspace, "GP1-XS")
-   size                = lookup(var.k8s_nodepool_size, terraform.workspace, 1)
+   name                = "default"
+   node_type           = lookup(var.k8s_default_nodepool_flavor, terraform.workspace, "GP1-XS")
+   size                = lookup(var.k8s_default_nodepool_size, terraform.workspace, 1)
    wait_for_pool_ready = true
+}
+
+
+resource "scaleway_k8s_pool" "jibri" {
+   autohealing         = lookup(var.k8s_nodepool_autohealing, terraform.workspace, true)
+   autoscaling         = lookup(var.k8s_jibri_nodepool_autoscale, terraform.workspace, true)
+   cluster_id          = scaleway_k8s_cluster.kube_cluster.id
+   container_runtime   = lookup(var.k8s_nodepool_container_runtime, terraform.workspace, "containerd")
+   max_size            = lookup(var.k8s_jibri_nodepool_max_nodes, terraform.workspace, 5)
+   min_size            = lookup(var.k8s_jibri_nodepool_min_nodes, terraform.workspace, 1)
+   name                = "jibri"
+   node_type           = lookup(var.k8s_jibri_nodepool_flavor, terraform.workspace, "GP1-S")
+   size                = lookup(var.k8s_jibri_nodepool_size, terraform.workspace, 1)
+   wait_for_pool_ready = false
+
+   # We wait for default pool to be ready before creating the jibri pool,
+   # otherwise some kube-system pods created by scaleway might be scheduled
+   # on the jibri pool at cluster initialization
+   depends_on = [ scaleway_k8s_pool.default ]
 }
 
 output "kubeconfig" {
