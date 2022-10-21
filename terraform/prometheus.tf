55f42f1a9d5bfec63f76a3988944fde460990d66
Manuel Raynaud <manu@raynaud.io>
2022-10-21 10:54:30 +0200

    ⬆️(prometheus) upgrade to version 1.2.0
    https://github.com/systemli/prometheus-jitsi-meet-exporter/
    releases/tag/1.2.0

--- terraform/prometheus.tf
+++ terraform/prometheus.tf
@@ -29,7 +29,7 @@ resource "helm_release" "kube-prometheus-stack" {
   }
 
   # Add Jitsi-Meet dashboards to Grafana
-  # Dashboards are fetched from https://github.com/systemli/prometheus-jitsi-meet-exporter/tree/1.1.9/dashboards
+  # Dashboards are fetched from https://github.com/systemli/prometheus-jitsi-meet-exporter/tree/1.2.0/dashboards
   # Values are merged with https://github.com/grafana/helm-charts/blob/main/charts/grafana/values.yaml
   set {
     name = "grafana.dashboardProviders.dashboardproviders\\.yaml.apiVersion"
@@ -53,7 +53,7 @@ resource "helm_release" "kube-prometheus-stack" {
 
   set {
     name = "grafana.dashboards.jitsi.jitsi-meet.url"
-    value = "https://raw.githubusercontent.com/systemli/prometheus-jitsi-meet-exporter/1.1.9/dashboards/jitsi-meet.json"
+    value = "https://raw.githubusercontent.com/systemli/prometheus-jitsi-meet-exporter/1.2.0/dashboards/jitsi-meet.json"
   }
 
   set {
@@ -63,7 +63,7 @@ resource "helm_release" "kube-prometheus-stack" {
 
   set {
     name = "grafana.dashboards.jitsi.jitsi-meet-system.url"
-    value = "https://raw.githubusercontent.com/systemli/prometheus-jitsi-meet-exporter/1.1.9/dashboards/jitsi-meet-system.json"
+    value = "https://raw.githubusercontent.com/systemli/prometheus-jitsi-meet-exporter/1.2.0/dashboards/jitsi-meet-system.json"
   }
 
   set {
